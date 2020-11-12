ceebd90b3d5476aacc5bd4d2557d7aee9f6499e1
Nwe Ni Kyaw <cunwenikyaw@gmail.com>
2020-11-12 20:29:47 +0630

    Created Multi Authentication - Admin Panel Login with Guard

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Hash;
+use Auth;
+use Session;
 
 class AdminController extends Controller
 {
@@ -13,9 +14,27 @@ class AdminController extends Controller
         return view('admin.admin_dashboard');
     }
 
-    public function login()
+    public function login(Request $request)
     {
         //echo $password = Hash::make(123456); die;
+        if($request->isMethod('post')){
+            $data = $request->all();
+            // echo "<pre>";
+            // print_r($data);
+            // die;
+            if(Auth::guard('admin')->attempt(['email'=>$data['email'], 'password'=>$data['password']])){
+                return redirect('admin/dashboard');
+            }else{
+                Session::flash('error_message', 'Invalid Email or Password');
+                return redirect()->back();
+            }
+        }
         return view('admin.admin_login');
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect('/admin');
     }
 }
